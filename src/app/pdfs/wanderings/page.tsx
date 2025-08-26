e83f504e35ac1c24134b61af7955b54b7de9c026
Sebastian Rios <iansebastos@Sebastians-MacBook-Air.local>
2025-08-26 00:38:02 -0700

    deployment 57

--- src/app/pdfs/wanderings/page.tsx
+++ src/app/pdfs/wanderings/page.tsx
@@ -4,13 +4,21 @@ import { useEffect } from 'react'
 
 export default function WanderingsRedirect() {
   useEffect(() => {
-    window.location.href = 'https://www.unrulyabstractions.com/pdf/wanderings.pdf'
+    // Immediate redirect
+    window.location.replace('https://www.unrulyabstractions.com/pdf/wanderings.pdf')
   }, [])
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <p>Redirecting to wanderings.pdf...</p>
-      <p>If you are not redirected automatically, <a href="https://www.unrulyabstractions.com/pdf/wanderings.pdf">click here</a>.</p>
-    </div>
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.location.replace('https://www.unrulyabstractions.com/pdf/wanderings.pdf');`
+        }}
+      />
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <p>Redirecting to wanderings.pdf...</p>
+        <p>If you are not redirected automatically, <a href="https://www.unrulyabstractions.com/pdf/wanderings.pdf">click here</a>.</p>
+      </div>
+    </>
   )
 }
